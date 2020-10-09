f6b0da251bcc2be251dcd6baa969d49bcf944c65
s_nakamura <s_nakamura@trancom.co.jp>
2020-10-09 16:38:45 +0900

    追加したモデルをサイトで使用する

--- django_md/qiiteru/views.py
+++ django_md/qiiteru/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post, Tag
 
 # Create your views here.
 def index(request):
-   return render(request, 'qiiteru/index.html')
+   posts = Post.objects.all().order_by('-created_at')
+   return render(request, 'qiiteru/index.html', {'posts': posts})
