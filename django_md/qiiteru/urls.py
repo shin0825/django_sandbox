71bdd29b37a80f1014343a4d679be5a4397a815c
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-10-28 23:32:13 +0900

    add add method

--- django_md/qiiteru/urls.py
+++ django_md/qiiteru/urls.py
@@ -5,4 +5,5 @@ app_name = 'qiiteru'
 urlpatterns = [
    path('', views.index, name='index'),
    path('detail/<int:post_id>/', views.detail, name='detail'),
+   path('add/', views.add, name='add'),
 ]
--- django_md/qiiteru/views.py
+++ django_md/qiiteru/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Tag
+from .forms import PostAddForm
 
 # Create your views here.
 def index(request):
@@ -12,3 +13,15 @@ def index(request):
 def detail(request, post_id):
    post = get_object_or_404(Post, id=post_id)
    return render(request, 'qiiteru/detail.html', {'post': post})
+
+def add(request):
+   if request.method == "POST":
+      form = PostAddForm(request.POST, request.FILES)
+      if form.is_valid():
+         post = form.save(commit=False)
+         post.user = request.user
+         post.save()
+         return redirect('qiiteru:index')
+   else:
+      form = PostAddForm()
+   return render(request, 'qiiteru/add.html', {'form': form})
