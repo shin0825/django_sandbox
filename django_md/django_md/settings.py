b5dc4d0a792b4d2a78022b708ab86783be33739a
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-11-03 01:46:59 +0900

    追加 bootstrap

--- django_md/django_md/settings.py
+++ django_md/django_md/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'qiiteru',
     'django_cleanup',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'bootstrap4.templatetags.bootstrap4',
+            ]
         },
     },
 ]
